1d6f673070bd407b27a267593b95b8079640f894
AenishShrestha <84612270+AenishShrestha@users.noreply.github.com>
2023-03-27 21:33:21 +0545

    test

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import pytube
+import base64
+from io import BytesIO
+
 
 video_url = st.text_input("Enter URL:")
 if video_url:
@@ -7,8 +10,19 @@ if video_url:
     video_id = video_instance.video_id
     video_url = f"https://www.youtube.com/watch?v={video_id}"
     st.video(video_url)
+    file_name =f'{video_instance.title}'
+    st.write(file_name)
     if st.button("Download Video"):
         st.success("File will be downloaded shortly.")
         stream = video_instance.streams.get_highest_resolution()
-        stream.download()
-        st.success("Video downloaded successfully!")
+        title = 'test.mp4'
+        video_file = stream.download(filename=title)
+        with open(title, 'rb') as f:
+            bytes = f.read()
+            b64 = base64.b64encode(bytes).decode()
+            href = f'<a href="data:file/zip;base64,{b64}" download=\'{title}\'>\
+                Here is your link \
+            </a>'
+            st.markdown(href, unsafe_allow_html=True)
+
+        st.success("Your File Is Ready To Be Downloaded!")
